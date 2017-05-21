611fd61d4eeaca4ab1b457299808ce2ae52c7c75
Andreas Doebeling <ad@1601.com>
2017-05-21 19:59:17 +0200

    Refactoring build-process

--- scripts/updatePhpDoc.sh
+++ scripts/updatePhpDoc.sh
@@ -2,4 +2,8 @@
 
 php7-70STABLE-CLI ../vendor/bin/phpdoc --directory=../class --target=. --template="../vendor/cvuorinen/phpdoc-markdown-public/data/templates/markdown-public" --title="PHP-Documentation of the Agenturtools" --cache-folder=../tmp/
 
-mv README.md ../Documentation.md
+rm ../phpDocumentation.md
+mv README.md ../phpDocumentation.md
+
+git add ../phpDocumentation.md
+git commit -m "Updating the auto-generated phpDoc"
