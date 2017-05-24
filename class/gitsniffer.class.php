2abe0cd5f568897fad64145cda585a1568139d6a
Andreas Doebeling <ad@1601.com>
2017-05-24 18:59:18 +0200

    Adding notification-field to gitSniffer

--- class/gitsniffer.class.php
+++ class/gitsniffer.class.php
@@ -202,6 +202,7 @@ class gitSniffer
                 'ip' => hostname::getIpByHostname(),
                 'gitStatus' => $this->getGitStatusAsMarkdown(),
                 'gitDiff' => $this->getGitDiffAsMarkdown(),
+                'notification' => reminderNotification,
             ]);
     }
 
